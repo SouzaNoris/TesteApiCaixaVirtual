d877f8334138cfe5fcfa673df5df4d2aa88e1f5a
Diogo Souza <diogo.theos@outlook.com>
2020-05-28 03:36:16 -0300

    Implementado usuario admin.

--- frontend/TesteApiCaixaVirtural/src/app/login/login.component.ts
+++ frontend/TesteApiCaixaVirtural/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { UsuarioService } from "../cadastro-usuario/usuario.service";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
+import { Usuario } from "../models/usuario";
 
 @Component({
   selector: 'app-login',
@@ -12,10 +13,10 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup;
 
   constructor(private usuarioService: UsuarioService,
-              private formBuilder: FormBuilder,
-              private routers: Router) {
+    private formBuilder: FormBuilder,
+    private routers: Router) {
   }
-  
+
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       usuario: ['', Validators.required],
@@ -25,7 +26,7 @@ export class LoginComponent implements OnInit {
 
   classError: string = 'danger';
 
-  usuario: string;s
+  usuario: string; s
   senha: string;
   showMessage = false;
   message = '';
@@ -34,16 +35,21 @@ export class LoginComponent implements OnInit {
     let usuario = this.loginForm.get('usuario').value;
     let senha = this.loginForm.get('senha').value;
 
-    this.usuarioService.search({ usuario, senha}).subscribe((login) => {
-          if (login.length) {
-             this.routers.navigateByUrl('home');
-             localStorage.setItem('usuario', login[0].usuario);
-             localStorage.setItem('idLoja', login[0].idLoja);
-          }
-          else {
-            this.message = 'Usuário ou senha informados são inválidos. Tente novamente.'
-            this.showMessage = true;
-          }
+    this.usuarioService.search({ usuario, senha }).subscribe((login) => {
+      if (login.length) {
+        this.routers.navigateByUrl('home');
+        localStorage.setItem('usuario', login[0].usuario);
+        localStorage.setItem('idLoja', login[0].idLoja);
+      }
+      else if (usuario === 'admin' && senha === 'admin') {
+        this.routers.navigateByUrl('home');
+        localStorage.setItem('usuario', 'admin');
+        localStorage.setItem('idLoja', 'admin');
+      }
+      else {
+        this.message = 'Usuário ou senha informados são inválidos. Tente novamente.'
+        this.showMessage = true;
+      }
     },
       (error) => {
         console.log(error);
